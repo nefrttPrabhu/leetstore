da992c5e274f3502bb71e207ee212e20a9b72e5b
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-12 23:12:05 +0530

    Time: 3 ms (32.09%) | Memory: 13.9 MB (20.89%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-
-        for(int i = 1; i < n - 1; i += 3){
-            if(nums[i] != nums[i-1]){
-                return nums[i - 1]; //beech mai koi 
+        unordered_map<int, int> mp;
+        for (int n : nums) {
+            mp[n]++;
+        }
+        
+        for (auto& [key, value] : mp) {
+            if (value == 1) {
+                return key;
             }
         }
-        return nums[n - 1]; //if no beech ka then automatically last one
+        
+        return -1; 
     }
 };
