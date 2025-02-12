09e3135d01bad6d9be311da1ace304c5f96934a3
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-12 17:55:24 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (86.93%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -6,9 +6,9 @@ public:
 
         for(int i = 1; i < n - 1; i += 3){
             if(nums[i] != nums[i-1]){
-                return nums[i - 1];
+                return nums[i - 1]; //beech mai koi 
             }
         }
-        return nums[n - 1];
+        return nums[n - 1]; //if no beech ka then automatically last one
     }
 };
