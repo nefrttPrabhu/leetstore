ca3cc784aaa2da9d19c2625c1a6d7ca020de2126
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-14 04:18:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (95.88%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <queue>
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(), stones.end()); //insert without using for loop for each of the stone in given vector
+
+        while (pq.size() > 1) {
+            int stone1 = pq.top();
+            pq.pop();
+            
+            int stone2 = pq.top(); 
+            pq.pop();
+            
+            if (stone1 != stone2) {
+                pq.push(stone1 - stone2);
+            }
+        }
+
+        return pq.empty() ? 0 : pq.top();
+    }
+};
