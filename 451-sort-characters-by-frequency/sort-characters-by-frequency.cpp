da797afc5ebf6581f3e6f8eb00bbc47341808a26
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-14 02:46:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (17.92%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -2,27 +2,60 @@
         // sort(s.rbegin(),s.rend());            //descending sort for string only
         // sort(s.begin(),s.end(),greater<int>); //descending sort for array only
 
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         string result;
+//         unordered_map<char, int> mp;
+//         priority_queue<pair<int, char>> pq;
+
+//         for (char ch : s) {
+//             mp[ch]++;
+//         }
+
+//         for (auto [ch, freq] : mp) {
+//             pq.push({freq, ch});
+//         }
+        
+//         while (!pq.empty()) {
+//             auto [freq, ch] = pq.top();
+//             pq.pop();
+//             result.append(freq, ch); 
+//         }
+
+//         return result;
+//     }
+// };
+
+
+typedef pair<char, int> p;
+struct lambda{
+    bool operator()(p &p1, p &p2){
+        return p1.second < p2.second; //max heap
+    }
+};
+
 class Solution {
 public:
     string frequencySort(string s) {
-        string result;
+        priority_queue<p, vector<p>, lambda> pq;
         unordered_map<char, int> mp;
-        priority_queue<pair<int, char>> pq;
 
-        for (char ch : s) {
+        for(char &ch : s){
             mp[ch]++;
         }
 
-        for (auto [ch, freq] : mp) {
-            pq.push({freq, ch});
-        }
-        
-        while (!pq.empty()) {
-            auto [freq, ch] = pq.top();
-            pq.pop();
-            result.append(freq, ch); 
+        for(auto it : mp){
+            pq.push({it.first, it.second});
         }
 
-        return result;
+        string res = "";
+
+        while(!pq.empty()){
+            p temp = pq.top();
+            pq.pop();
+            res += string(temp.second, temp.first);
+        }
+        return res;
     }
 };
