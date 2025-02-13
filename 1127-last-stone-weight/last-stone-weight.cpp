d3a056821a419407b7ee85a6342f3c8aeb2121ff
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-14 04:20:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (95.88%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,20 +1,41 @@
+// class Solution {
+// public:
+//     int lastStoneWeight(vector<int>& stones) {
+//         priority_queue<int> pq(stones.begin(), stones.end()); //insert without using for loop for each of the stone in given vector
+
+//         while (pq.size() > 1) {
+//             int stone1 = pq.top();
+//             pq.pop();
+            
+//             int stone2 = pq.top(); 
+//             pq.pop();
+            
+//             if (stone1 != stone2) {
+//                 pq.push(stone1 - stone2);
+//             }
+//         }
+
+//         return pq.empty() ? 0 : pq.top();
+//     }
+// };
+
+
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> pq(stones.begin(), stones.end()); //insert without using for loop for each of the stone in given vector
+        while (stones.size() > 1) {
+            sort(stones.begin(), stones.end());
+            
+            int stone1 = stones.back(); 
+            stones.pop_back();
 
-        while (pq.size() > 1) {
-            int stone1 = pq.top();
-            pq.pop();
-            
-            int stone2 = pq.top(); 
-            pq.pop();
-            
+            int stone2 = stones.back(); 
+            stones.pop_back();
+
             if (stone1 != stone2) {
-                pq.push(stone1 - stone2);
+                stones.push_back(stone1 - stone2);
             }
         }
-
-        return pq.empty() ? 0 : pq.top();
+        return stones.empty() ? 0 : stones[0];
     }
 };
