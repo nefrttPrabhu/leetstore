7c80271da1f18db2b1c423a54f154dfa1b55b1fb
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-16 04:39:18 +0530

    Time: 3 ms (43.89%) | Memory: 21.1 MB (17.84%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void dfs(vector<vector<int>>& adj, vector<int>& vis, int node){
-        for(auto it: adj[node]){
-                vis[node] = 1;
+        for(auto it: adj[node]){//idhar agar vis[node] = 1 lagate then bus baar baar overwrite karta rehta of the visited nodes coz vis not vis ka check toh neeche rakh he lega
             if(!vis[it]){
+                vis[node] = 1;
                 dfs(adj, vis, it);
             }
         }
@@ -26,8 +26,8 @@ public:
 
         for(int i = 0 ;i < l; i++){
             if(!vis[i]){
+                dfs(adj,vis,i);//idhar waiting mai hi jab sare iterations us starting node(provence) ke complete ho gye then fng return ata ie ek province count ho gya 
                 cnt++;
-                dfs(adj,vis,i);
             }
         }
         return cnt;
