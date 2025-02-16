2cd3bec6217300edd7218652d3725c4b87df01cc
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-16 16:31:15 +0530

    Time: 70 ms (35.15%) | Memory: 124.9 MB (74.96%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,7 +1,44 @@
+// class MedianFinder {
+// private:
+//     priority_queue<int> left; // Max heap for the smaller half
+//     priority_queue<int, vector<int>, greater<int>> right; // Min heap for the larger half
+//     vector<int> arr; // Stores numbers
+
+// public:
+//     MedianFinder() {}
+
+//     void addNum(int num) {
+//         arr.push_back(num); // Store the number
+//         int n = arr.size();
+
+//         // Push all elements into the right heap
+//         for (int i = 0; i < n; i++) {
+//             right.push(arr[i]);
+//         }
+
+//         // Move half of the elements from right heap to left heap
+//         while (left.size() < n / 2) {
+//             left.push(right.top());
+//             right.pop();
+//         }
+//     }
+
+//     double findMedian() {
+//         int n = arr.size();
+//         if (n % 2 == 0) {
+//             return (left.top() + right.top()) / 2.0; // Floating-point division
+//         } else {
+//             return left.top();
+//         }
+//     }
+// };
+
+
+
 class MedianFinder {
 private:
-    priority_queue<int> left; // Max heap (stores smaller half)
-    priority_queue<int, vector<int>, greater<int>> right; // Min heap (stores larger half)
+    priority_queue<int> left; 
+    priority_queue<int, vector<int>, greater<int>> right; 
 
 public:
     MedianFinder() {}
@@ -21,7 +58,7 @@ public:
             right.pop();
         }
     }
-    
+    //global declare kiye coz addnum toh void hi toh return nhi karega and findmedian he return karega toh global acessable hi toh udhar se he operations karke return kardega
     double findMedian() {
         if (left.size() > right.size()) {
             return left.top(); 
