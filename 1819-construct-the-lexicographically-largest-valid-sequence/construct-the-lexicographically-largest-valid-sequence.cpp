5f108d4e620c055bf828544e73da2318402d0dff
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-17 13:20:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (19.13%) - LeetSync

--- 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.cpp
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.cpp
@@ -1,52 +1,45 @@
 class Solution {
 public:
-    vector<int> result;
-    vector<bool> used;
-
-    bool backtrack(vector<int>& seq, int index, int n) {
-        if (index == seq.size()) 
+    bool fng(vector<int>& vis, vector<int>& ans, int index, int n) {
+        if (index == ans.size()) {
             return true;
-            
-        if (seq[index] != 0) 
-            return backtrack(seq, index + 1, n);
+        }
 
-        for (int num = n; num >= 1; --num) {
-            if (used[num]) 
-                continue;
+        if (ans[index] != -1) {
+            return fng(vis, ans, index + 1, n);
+        }
 
-            if (num == 1) {
-                seq[index] = 1;
-                used[1] = true;
+        for (int nums = n; nums > 0; nums--) {
+            if (!vis[nums]) {
+                vis[nums] = true;
+                ans[index] = nums;
 
-                if (backtrack(seq, index + 1, n)) 
-                    return true;
-
-                seq[index] = 0;
-                used[1] = false;
-            } 
-            else {
-                int secondIndex = index + num;
-                if (secondIndex < seq.size() && seq[secondIndex] == 0) {
-                    seq[index] = seq[secondIndex] = num;
-                    used[num] = true;
-
-                    if (backtrack(seq, index + 1, n)) 
+                if (nums == 1) {
+                    if (fng(vis, ans, index + 1, n)) {
                         return true;
-
-                    seq[index] = seq[secondIndex] = 0;
-                    used[num] = false;
+                    }
+                } else {
+                    // Check if we can place the second occurrence of the number
+                    if (index + nums < ans.size() && ans[index + nums] == -1) {
+                        ans[index + nums] = nums;
+                        if (fng(vis, ans, index + 1, n)) {
+                            return true;
+                        }
+                        ans[index + nums] = -1;
+                    }
                 }
+
+                ans[index] = -1;
+                vis[nums] = false;
             }
         }
         return false;
     }
 
     vector<int> constructDistancedSequence(int n) {
-        int size = 2 * (n - 1) + 1;
-        vector<int> seq(size, 0);
-        used.resize(n + 1, false);
-        
-        backtrack(seq, 0, n);
-        return seq;
+        vector<int> vis(n + 1, false); 
+        vector<int> ans(2 * n - 1, -1); 
+        fng(vis, ans, 0, n);
+        return ans;
     }
 };
