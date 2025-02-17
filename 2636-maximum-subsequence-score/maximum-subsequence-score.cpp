25f78f119cad7b037996ae50b8179eac38c00579
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-17 22:52:40 +0530

    Time: 74 ms (36.35%) | Memory: 99.1 MB (32.96%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.cpp
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.cpp
@@ -2,42 +2,35 @@ class Solution {
 public:
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
         int n = nums1.size();
-        
-        vector<pair<int,int>> vec(n);
-        
-        for(int i = 0; i<n; i++) {
-            vec[i] = {nums1[i], nums2[i]};
+        vector<pair<int, int>> pairs;
+
+        // Step 1: Pair nums2[i] with nums1[i] and sort by nums2[i] in descending order
+        for (int i = 0; i < n; i++) {
+            pairs.emplace_back(nums2[i], nums1[i]);
         }
-        
-        auto lambda = [&](auto &P1, auto &P2) {
-            return P1.second > P2.second;
-        };
-        
-        sort(begin(vec), end(vec), lambda);
-        
-        priority_queue<int, vector<int>, greater<int>> pq; //min_heap
-        
-        long long Ksum = 0;
-        
-        for(int i = 0; i<=k-1; i++) {
-            
-            Ksum += vec[i].first;
-            pq.push(vec[i].first);
-            
+        sort(pairs.rbegin(), pairs.rend()); // Sort by nums2[i] descending
+
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        long long sum = 0, maxScore = 0;
+
+        // Step 2: Iterate through sorted nums2 while maintaining a heap of k elements from nums1
+        for (int i = 0; i < n; i++) {
+            int num2 = pairs[i].first;
+            int num1 = pairs[i].second;
+
+            sum += num1;
+            minHeap.push(num1);
+
+            if (minHeap.size() > k) {
+                sum -= minHeap.top();
+                minHeap.pop();
+            }
+
+            if (minHeap.size() == k) {
+                maxScore = max(maxScore, sum * num2);
+            }
         }
-        
-        long long result = Ksum * vec[k-1].second;
-        
-        for(int i = k; i<n; i++) {
-            Ksum += vec[i].first - pq.top();
-            pq.pop();
-            
-            pq.push(vec[i].first);
-            
-            result = max(result, Ksum * vec[i].second);
-            
-        }
-        
-        return result;
+
+        return maxScore;
     }
 };
