8159d8260c0d2a571480a51363c19b593903369f
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-19 13:02:56 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (87.05%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -69,26 +69,58 @@
 
 
 
+// class Solution {
+// public:
+//     void generateHappyStrings(int n, string current, vector<string> &result) {
+//         if (current.length() == n) {
+//             result.push_back(current);
+//             return;
+//         }
+        
+//         for (char ch : {'a', 'b', 'c'}) {
+//             if (current.empty() || current.back() != ch) {
+//                 generateHappyStrings(n, current + ch, result);
+//             }
+//         }
+//     }
+
+//     string getHappyString(int n, int k) {
+//         vector<string> happy;
+//         generateHappyStrings(n, "", happy);
+        
+//         if (k > happy.size()) return "";
+//         return happy[k - 1]; // k is 1-based index
+//     }
+// };
+
+
 class Solution {
 public:
-    void generateHappyStrings(int n, string current, vector<string> &result) {
-        if (current.length() == n) {
-            result.push_back(current);
-            return;
+    string ans = "";
+    vector<char> v{'a','b','c'};
+    bool f(int ind,string &s,int &k){
+        if(ind == s.size()){
+            k--;
+            if(k == 0){
+                ans = s;
+                return true;
+            }
+            return false;
         }
-        
-        for (char ch : {'a', 'b', 'c'}) {
-            if (current.empty() || current.back() != ch) {
-                generateHappyStrings(n, current + ch, result);
+        for(auto &ch : v){
+            if(ind == 0 || (ind - 1 >= 0 && s[ind - 1] != ch)){
+                s[ind] = ch;
+                bool dd = f(ind + 1, s, k);
+                if(dd) return true;
+                s[ind] = '.';
             }
         }
+        return false;
     }
 
     string getHappyString(int n, int k) {
-        vector<string> happy;
-        generateHappyStrings(n, "", happy);
-        
-        if (k > happy.size()) return "";
-        return happy[k - 1]; // k is 1-based index
+        string s(n, '.');
+        f(0, s, k);
+        return ans;
     }
 };
