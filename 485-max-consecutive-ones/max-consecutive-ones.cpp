cd4646eb7ef9b71ce1ce06acac9a9529c005d8ca
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-21 01:35:33 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (29.34%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -4,15 +4,17 @@ public:
         int l = nums.size();
         int c = 0;
         int maxi = 0;
+
         for(int i = 0;i < l; i++){
             if(nums[i] == 1){
                 c++;
-                maxi = max(maxi,c);
             }
+
             else{
                 c = 0;
             }
+                maxi = max(maxi,c);
         }
-        return max(maxi,c);
+        return maxi;
     }
 };
