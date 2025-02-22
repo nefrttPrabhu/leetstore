eece8d2e57565570ed3161e44568a30175ac34b6
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-22 22:18:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.59%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        long long exp = n; 
+        if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+
+        double result = 1.0;
+        while (exp > 0) {
+            if (exp % 2 == 1) { 
+                result *= x;
+            }
+            x *= x;
+            exp /= 2; 
+        }
+    
+        return result;
+    }
+};
