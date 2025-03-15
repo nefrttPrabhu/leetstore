3ad4f07682785cde95d19971485ea5729eb2a579
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-03-15 23:06:33 +0530

    Time: 6 ms (65.64%) | Memory: 128.4 MB (58.76%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -4,19 +4,19 @@ public:
         int n = nums.size();
         int pos = 0;
         int neg = 1;
-        vector<int> temp(n, 0);
+        vector<int> ans(n, 0);
 
         for(int i = 0; i < n; i++){
             if(nums[i] > 0){
-                temp[pos] = nums[i];
+                ans[pos] = nums[i];
                 pos += 2;
             }
 
             else{
-                temp[neg] = nums[i];
+                ans[neg] = nums[i];
                 neg += 2;
             }
         }
-        return temp;
+        return ans;
     }
 };
