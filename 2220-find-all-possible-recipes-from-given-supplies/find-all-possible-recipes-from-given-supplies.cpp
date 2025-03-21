d0724da2d2e10cc397b54886a21115015c6bf2ca
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-03-21 06:42:02 +0530

    Time: 103 ms (95.02%) | Memory: 165.5 MB (27.84%) - LeetSync

---
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        unordered_map<string, int> inDegree;
+        unordered_map<string, vector<string>> graph;
+        unordered_set<string> available(supplies.begin(), supplies.end());
+        queue<string> q;
+
+        for (int i = 0; i < recipes.size(); i++) {
+            inDegree[recipes[i]] = ingredients[i].size();
+            for (const string& ing : ingredients[i]) {
+                graph[ing].push_back(recipes[i]);
+            }
+        }
+
+        for (const string& s : supplies) {
+            q.push(s);
+        }
+
+        vector<string> result;
+
+        while (!q.empty()) {
+            string item = q.front();
+            q.pop();
+
+            if (inDegree.find(item) != inDegree.end()) {
+                result.push_back(item);
+            }
+
+            for (const string& recipe : graph[item]) {
+                if (--inDegree[recipe] == 0) {
+                    q.push(recipe);
+                }
+            }
+        }
+
+        return result;
+    }
+};
