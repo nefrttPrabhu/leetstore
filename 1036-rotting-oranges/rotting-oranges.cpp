792347a190835e3d815c7487cbc0357238fa7385
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-03-23 05:31:22 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (55.15%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    using p = pair<int,int>;
+    using p = pair<int, int>;
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        queue<p>q;
-        int time = -1;
-        int fresh = 0;
-        vector<vector<int>> dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        queue<p> q;
 
-        for(int i = 0;i < m; i++){
-            for(int j = 0;j < n; j++){
+        int fresh = 0;
+        int time = -1;
+
+        vector<vector<int>> dir = {{1,0}, {0,1}, {0,-1}, {-1,0}};
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(grid[i][j] == 1){
                     fresh++;
                 }
                 if(grid[i][j] == 2){
-                    q.push({i,j});
+                    q.push({i, j});
                 }
             }
         }
 
-        if(!fresh){
+        if(fresh == 0){
             return 0;
         }
 
-            while(!q.empty()){  
-                int size = q.size();
-                time++;
-                while(size--){ 
-                    auto node = q.front();
-                    q.pop();
-                    for(int i = 0;i < 4; i++){
+        while(!q.empty()){
+            int size = q.size();
+            time++;
+            while(size--){
+                auto node = q.front();
+                q.pop();
+                for(int i = 0; i < 4; i++){
                     int a = node.first + dir[i][0];
                     int b = node.second + dir[i][1];
                     if(a >= 0 && a < m && b >= 0 && b < n && grid[a][b] == 1){
