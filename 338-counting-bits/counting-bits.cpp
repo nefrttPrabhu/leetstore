94d4954dbcd4e49f07a1cea8284f31072a8b819f
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-03-26 14:10:00 +0530

    Time: 1 ms (37.69%) | Memory: 11.2 MB (31.42%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n + 1);
-        for(int i = 0; i < n + 1; i++){
-            ans[i] = __builtin_popcount(i);
+        vector<int> ans;
+        
+        for(int i = 0; i <= n; i++) {
+            int sum = 0;
+            int num = i;
+            
+            while(num != 0) {
+                sum += num % 2;
+                num = num / 2;
+            }
+            
+            ans.push_back(sum);
         }
+        
         return ans;
     }
 };
