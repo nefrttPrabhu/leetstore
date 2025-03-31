8785762cf90305cf1129c26955e453a001d482cf
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-03-31 22:56:41 +0530

    Time: 39 ms (43.88%) | Memory: 67.5 MB (56.54%) - LeetSync

---
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        int n = weights.size();
+        if (k == 1) return 0; 
+
+        vector<int> pairSums;
+        for (int i = 0; i < n - 1; ++i) {
+            pairSums.push_back(weights[i] + weights[i + 1]);
+        }
+
+        sort(pairSums.begin(), pairSums.end());
+
+        long long minSum = 0, maxSum = 0;
+        for (int i = 0; i < k - 1; ++i) {
+            minSum += pairSums[i];
+            maxSum += pairSums[n - 2 - i];
+        }
+
+        return maxSum - minSum;
+    }
+};
