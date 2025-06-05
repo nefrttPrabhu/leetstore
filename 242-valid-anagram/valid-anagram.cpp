63d7e9c1b770d2e5d2a3cb11f862fb549c9c9490
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-06-05 20:04:50 +0530

    Time: 3 ms (53.08%) | Memory: 9.7 MB (74.39%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if (s.length() != t.length()){
+            return false;
+        }
+        
+        int n = s.length();
+
+        map<char, int> a, b;
+
+        for (int i = 0; i < n; i++) { 
+            a[s[i]]++;
+            b[t[i]]++;
+        }
+        
+        for (char c = 'a'; c <= 'z'; c++){
+            if (a[c] != b[c]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
