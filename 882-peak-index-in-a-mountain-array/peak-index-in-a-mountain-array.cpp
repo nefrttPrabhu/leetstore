fedd3a90ac63b256126cb9be43b4c987efc56c2d
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-06-13 15:58:21 +0530

    Time: 0 ms (100.00%) | Memory: 63.7 MB (11.98%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -7,10 +7,12 @@ public:
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
-            if (arr[mid] < arr[mid + 1])
+            if (arr[mid] < arr[mid + 1]){
                 low = mid + 1;
-            else
+            }
+            else{
                 high = mid - 1;
+            }
         }
         return low;
     }
