6470719ed19f45ab80867133ee7942fa59770805
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-06-20 01:10:35 +0530

    Time: 3 ms (43.01%) | Memory: 20.4 MB (44.36%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj, vector<int>& vis, int node){
-        for(auto it: adj[node]){//idhar agar vis[node] = 1 lagate then bus baar baar overwrite karta rehta of the visited nodes coz vis not vis ka check toh neeche rakh he lega
-            if(!vis[it]){
-                vis[node] = 1;//idhar seedha bus non iterated walon pe 1 update hoga
+    void dfs(vector<vector<int>>& adj, vector<int>& vis, int node) {
+        vis[node] = 1;
+        for (auto it : adj[node]) {
+            if (!vis[it]) {
                 dfs(adj, vis, it);
             }
         }
     }
 
-    int findCircleNum(vector<vector<int>>& a) {
-        int l = a.size();
-        vector<vector<int>> adj(l);
-        vector<int> vis(l,0);
-        int cnt = 0;
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<vector<int>> adj(n);
 
-        for(int i = 0;i < l; i++){
-            for(int j = 0;j < l; j++){
-                if(a[i][j] == 1){
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j] == 1) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
             }
         }
 
-        for(int i = 0 ;i < l; i++){
-            if(!vis[i]){
-                dfs(adj,vis,i);//idhar waiting mai hi jab sare iterations us starting node(provence) ke complete ho gye then fng return ata ie ek province count ho gya 
+        vector<int> vis(n, 0);
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
                 cnt++;
+                dfs(adj, vis, i);
             }
         }
+
         return cnt;
     }
 };
