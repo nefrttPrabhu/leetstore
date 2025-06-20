a2a532b1c8c81434d8f02a34e9eaf6ce0b4005aa
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-06-20 11:06:12 +0530

    Time: 29 ms (45.62%) | Memory: 24.3 MB (23.18%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,33 +1,50 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
 class Solution {
 public:
     int directions[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
-    void fng(vector<vector<int>>& vis, vector<vector<char>>& grid, int row, int col) {
-        int n = grid.size(), m = grid[0].size();
+    void bfs(vector<vector<int>>& vis, vector<vector<char>>& grid, int row, int col) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        queue<pair<int, int>> q;
+        q.push({row, col});
         vis[row][col] = 1;
 
-        for (int k = 0; k < 4; k++) {
-            int nr = row + directions[k][0];
-            int nc = col + directions[k][1];
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
 
-            if (nr >= 0 && nr < n && nc >= 0 && nc < m && vis[nr][nc] == 0 && grid[nr][nc] == '1') {
-                fng(vis, grid, nr, nc);
+            for (int k = 0; k < 4; k++) {
+                int nr = r + directions[k][0];
+                int nc = c + directions[k][1];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
+                    vis[nr][nc] == 0 && grid[nr][nc] == '1') {
+                    vis[nr][nc] = 1;
+                    q.push({nr, nc});
+                }
             }
         }
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size(), m = grid[0].size(), res = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        int cnt = 0;
         vector<vector<int>> vis(n, vector<int>(m, 0));
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (vis[i][j] == 0 && grid[i][j] == '1') {
-                    res++;
-                    fng(vis, grid, i, j);
+                    cnt++;
+                    bfs(vis, grid, i, j);
                 }
             }
         }
-        return res;
+        return cnt;
     }
 };
