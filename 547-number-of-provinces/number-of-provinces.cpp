65496c27ffa55a576dcbb292bc2cc5b1bc80832e
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-06-21 22:04:24 +0530

    Time: 6 ms (19.00%) | Memory: 20.8 MB (34.28%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj, vector<int>& vis, int node) {
-        vis[node] = 1;
-        for (auto it : adj[node]) {
-            if (!vis[it]) {
-                dfs(adj, vis, it);
+    void bfs(vector<vector<int>>& adj, vector<int>& vis, int start) {
+        queue<int> q;
+        q.push(start);
+        vis[start] = 1;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (auto it : adj[node]) {
+                if (!vis[it]) {
+                    vis[it] = 1;
+                    q.push(it);
+                }
             }
         }
     }
@@ -13,6 +22,7 @@ public:
         int n = isConnected.size();
         vector<vector<int>> adj(n);
 
+        // Convert adjacency matrix to adjacency list
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (isConnected[i][j] == 1) {
@@ -28,7 +38,7 @@ public:
         for (int i = 0; i < n; i++) {
             if (!vis[i]) {
                 cnt++;
-                dfs(adj, vis, i);
+                bfs(adj, vis, i);
             }
         }
 
