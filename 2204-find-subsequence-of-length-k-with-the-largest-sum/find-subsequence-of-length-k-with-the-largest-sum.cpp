292a88939feed35983b18dfb8ce52ba9a0b6cbd6
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-06-30 13:01:17 +0530

    Time: 3 ms (39.50%) | Memory: 14 MB (40.91%) - LeetSync

---
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    using p = pair<int, int>;
+    
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        priority_queue<p> pqmax;
+        priority_queue<p, vector<p>, greater<p>> pqmin;
+
+        for(int i = 0; i < nums.size(); i++) {
+            pqmax.push({nums[i], i}); 
+        }
+
+        // Transfer top k elements from max-heap to min-heap
+        int count = 0;
+        while(count < k && !pqmax.empty()) {
+            p top = pqmax.top(); pqmax.pop();
+            pqmin.push({top.second, top.first});
+            count++;
+        }
+
+        vector<int> ans;
+        while(!pqmin.empty()) {
+            ans.push_back(pqmin.top().second); // get the original number
+            pqmin.pop();
+        }
+
+        return ans;
+    }
+};
