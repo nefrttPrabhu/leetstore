908dd96c520cae8e7448be1c6f78d22598070af4
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-07-01 21:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (34.67%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,50 +1,48 @@
 class Solution {
 public:
-    using p = pair<int,int>;
+    using p = pair<int, int>;
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        vector<vector<int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
+        int n = grid.size();
+        int m = grid[0].size();
+        int fresh = 0;
+        int cnt = -1;
 
         queue<p> q;
 
-        int fresh = 0;
-        int time = -1;
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 if(grid[i][j] == 1){
                     fresh++;
                 }
                 if(grid[i][j] == 2){
-                    q.push({i , j});
+                    q.push({i, j});
                 }
             }
         }
 
-        if(fresh == 0){
-            return 0;
-        }
+        if(fresh == 0) return 0;
 
+        vector<vector<int>> dir = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
         while(!q.empty()){
             int size = q.size();
+            cnt++;
             while(size--){
-                auto node = q.front();
+                p idx = q.front();
                 q.pop();
-                for(int i = 0; i < 4; i++){
-                    int a = node.first + dir[i][0];
-                    int b = node.second + dir[i][1];
 
-                    if(a >= 0 && a < m && b >= 0 && b < n && grid[a][b] == 1){
+                for(int i = 0; i < 4; i++){
+                    int a = idx.first + dir[i][0];
+                    int b = idx.second + dir[i][1];
+
+                    if(a < n && a >= 0 && b < m && b >= 0 && grid[a][b] == 1){
                         grid[a][b] = 2;
                         q.push({a, b});
                         fresh--;
                     }
                 }
             }
-            time++;
         }
-        return fresh != 0 ? -1 : time;
+        if(fresh != 0) return -1;
+        return cnt;
     }
 };
