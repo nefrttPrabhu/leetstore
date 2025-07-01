a60f4c94aec6c34018d7916ca40c749d1318aee4
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-07-01 18:40:00 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (9.46%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -24,7 +24,7 @@ public:
 
         // Convert adjacency matrix to adjacency list
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+            for (int j = 0; j < n; j++) {
                 if (isConnected[i][j] == 1) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
