55e10ef564494ebb9b4db18314e63111ae2bfb47
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-07-02 11:18:39 +0530

    Time: 27 ms (58.70%) | Memory: 24.3 MB (23.57%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -12,7 +12,45 @@ public:
 
         while (!q.empty()) {
             auto [r, c] = q.front();
-            q.pop();
+            q.pop();class Solution {
+public:
+    int directions[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+    void dfs(vector<vector<int>>& vis, vector<vector<char>>& grid, int row, int col) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vis[row][col] = 1;
+
+        for (int k = 0; k < 4; k++) {
+            int nr = row + directions[k][0];
+            int nc = col + directions[k][1];
+
+            if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
+                vis[nr][nc] == 0 && grid[nr][nc] == '1') {
+                dfs(vis, grid, nr, nc);
+            }
+        }
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int cnt = 0;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (vis[i][j] == 0 && grid[i][j] == '1') {
+                    cnt++;
+                    dfs(vis, grid, i, j);
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
 
             for (int k = 0; k < 4; k++) {
                 int nr = r + directions[k][0];
