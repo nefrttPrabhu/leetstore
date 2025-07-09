fd5b64fab75c63793f792eaf5ab654dc03769de6
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-07-09 16:10:19 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (60.89%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    void generateSubsets(vector<int>& nums, int index, vector<int>& current, vector<vector<int>>& result){
-        if (index == nums.size()) {
-            result.push_back(current);
-            return;
+    void fng(vector<int>& nums, vector<vector<int>>& ans, vector<int>& res, int i){
+        if(i == nums.size()){
+            ans.push_back(res);
+            return ;
         }
 
-        current.push_back(nums[index]);
-        generateSubsets(nums, index + 1, current, result); // include 
+        res.push_back(nums[i]);
+        fng(nums, ans, res, i + 1);
 
-        current.pop_back();
-        generateSubsets(nums, index + 1, current, result);//exclude
+        res.pop_back();
+        fng(nums, ans, res, i + 1);
     }
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> current;
-        generateSubsets(nums, 0, current, result);
-        return result;
+        vector<vector<int>> ans;
+        vector<int> res;
+        int i = 0;
+        fng(nums, ans, res, i);
+        return ans;
     }
-
 };
