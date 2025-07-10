f8516f98bd9db180e731ddd1520758315d322c6e
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-07-11 01:55:49 +0530

    Time: 3 ms (56.28%) | Memory: 14 MB (73.13%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void fng(int ind, vector<int>& arr, int tar, vector<vector<int>>& ans, vector<int>& ds) {
+        if (ind == arr.size()) {
+            if (tar == 0) {
+                ans.push_back(ds);
+            }
+            return;
+        }
+
+        if (arr[ind] <= tar) {
+            ds.push_back(arr[ind]);
+            fng(ind, arr, tar - arr[ind], ans, ds);
+            ds.pop_back();
+        }
+
+        fng(ind + 1, arr, tar, ans, ds);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int tar) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        fng(0, candidates, tar, ans, ds);
+        return ans;
+    }
+};
