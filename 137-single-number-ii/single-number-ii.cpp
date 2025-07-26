1789dc8ff31c6e76683ad7330ce532495f7b7bed
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-07-26 20:27:03 +0530

    Time: 4 ms (14.95%) | Memory: 14.1 MB (10.69%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ones = 0, twos = 0;
-        for (int num : nums) {
-            ones = (ones ^ num) & ~twos;
-            twos = (twos ^ num) & ~ones;
+        map<int,int> mp;
+
+        for(auto it : nums){
+            mp[it]++;
         }
-        return ones;
+
+        for(auto it : mp) if(it.second == 1) return it.first;
+        return 0;
     }
 };
