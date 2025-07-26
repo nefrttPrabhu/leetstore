e06000e7a8deeab502069b236ac4c7eefdc31130
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-07-26 15:58:11 +0530

    Time: 4 ms (11.08%) | Memory: 27.3 MB (20.11%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -3,25 +3,23 @@ public:
     int trap(vector<int>& height) {
         vector<int>pg(height.size(),0);
         vector<int>ng(height.size(),0);
+        int n = height.size();
 
-        //prev greater
         pg[0] = height[0];
-        for(int i = 1; i < height.size(); i++){
+        for(int i = 1; i < n; i++){
             pg[i] = max(pg[i - 1], height[i]);
         }
 
-        //next greater 
-        ng[height.size() - 1] = height[height.size() - 1];
-        for(int i = height.size() - 2; i >= 0; i--){
+        ng[n - 1] = height[n - 1];
+        for(int i = n - 2; i >= 0; i--){
             ng[i] = max(ng[i + 1], height[i]);
         }
 
-        //water
-        int sum = 0;
-        for(int i = 0; i < height.size(); i++){
-            sum += min(pg[i],ng[i]) - height[i];
-        }
+        int water = 0;
 
-        return sum;
+        for(int i = 0; i < n; i++){
+            water += (min(pg[i],ng[i]) - height[i]);
+        }
+        return water;
     }
 };
