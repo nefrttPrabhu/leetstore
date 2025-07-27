299685b6415b37caea50931f0cc09a607aaedd8b
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-07-27 11:56:56 +0530

    Time: 0 ms (100.00%) | Memory: 100.3 MB (66.18%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        if (nums.size() <= 1) return true;
+        if(nums.size() == 1) return true;
 
-        stack<int> st;
-        st.push(nums[0]);
+        int a = nums[0];
+        int b = nums[nums.size() - 1];
 
-        int flg = 0; 
-
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == st.top()) {
-                st.push(nums[i]);
-                continue;
-            }
-
-            if (flg == 0) {
-                flg = (nums[i] > st.top()) ? 1 : -1;
-                st.push(nums[i]);
-            } 
-            else if (flg == 1) { 
-                if (nums[i] < st.top()) return false;
-                st.push(nums[i]);
-            } 
-            else { 
-                if (nums[i] > st.top()) return false;
-                st.push(nums[i]);
+        if(a <= b){ //increasing 
+            for(int i = 0; i < nums.size() - 1; i++){
+                if(nums[i + 1] < nums[i]) {
+                    return false;
+                }
             }
         }
-
+        else{ //decreasing
+            for(int i = 0; i < nums.size() - 1; i++){
+                if(nums[i + 1] > nums[i]) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 };
