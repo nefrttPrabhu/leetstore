7f16ea8e771631f7f42d7452f3dd062fffc44b52
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-07-27 22:59:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (69.06%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int helper(int row, int col, vector<vector<int>> &dp) {
-        // base case
         if(row == 0 && col == 0) return 1;
-        if(row < 0 || col < 0) return 0;
+        // if(row < 0 || col < 0) return 0;
         if(dp[row][col] != -1) return dp[row][col];
-
-        int right = helper(row, col - 1, dp);
-
-        int down = helper(row - 1, col, dp);
-
+        int right = 0;
+        if(col > 0) right = helper(row, col - 1, dp);
+        int down = 0;
+        if(row > 0) down = helper(row - 1, col, dp);
         return dp[row][col] = right + down;
     }
-
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int> (n, -1));
         return helper(m - 1, n - 1, dp);
