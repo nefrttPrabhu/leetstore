5136b3814280bad72559aa7777c15be7ba600252
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-08-01 13:27:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (5.85%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,44 +1,43 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        if(!root) return {};
-        queue<TreeNode*> q;
         vector<vector<int>> ans;
-        int flag = 0;
-    
-        q.push(root);
+        if (!root) return ans;
 
-        while(!q.empty()) {
-            int size = q.size();
-            vector<int> temp;
+        queue<TreeNode*> q;
+        q.push(root);
+        int c = 0;
+
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> res; 
+            stack<int> st;  
             
-            while(size--) {
-                temp.push_back(q.front()->val);
-                if(q.front()->left) {
-                    q.push(q.front()->left);
-                }
-                if(q.front()->right) {
-                    q.push(q.front()->right);
-                }
+            for (int i = 0; i < n; ++i) {
+                TreeNode* temp = q.front();
                 q.pop();
+
+                if (c % 2 == 0) {
+                    res.push_back(temp->val);
+                } else {
+                    st.push(temp->val);
+                }
+
+                if (temp->left) q.push(temp->left);
+                if (temp->right) q.push(temp->right);
             }
-            if(flag % 2 == 1) {
-                reverse(temp.begin(), temp.end());
-            } 
-            ans.push_back(temp);
-            flag++;
+
+            if (c % 2 == 1) {
+                while (!st.empty()) {
+                    res.push_back(st.top());
+                    st.pop();
+                }
+            }
+
+            ans.push_back(res);
+            c = !c; 
         }
+
         return ans;
     }
 };
