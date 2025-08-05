0d495c1c5281a7871c0ffa840953b90f7a5f548f
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-08-05 19:08:44 +0530

    Time: 3 ms (17.07%) | Memory: 11.9 MB (7.96%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        stack<int> st;
+        vector<int> ans;
+
+        for(int i = 0; i <= s.size(); i++){
+            st.push(i);
+            if(i == s.size() || s[i] == 'I'){
+                while(!st.empty()){
+                    ans.push_back(st.top());
+                    st.pop();
+                }
+            }
+        }
+        return ans;
+    }
+};
