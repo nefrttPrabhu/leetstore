4b12e46e776b5d9ee4c9fe52859c379373d473fd
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-08-10 11:10:03 +0530

    Time: 407 ms (8.00%) | Memory: 203.6 MB (9.23%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        vector<int> olost;
+        vector<int> nlost;
+        set<int> st;
         map<int, int> mp;
 
-        for (int i = 0; i < matches.size(); i++) {
-            mp[matches[i][1]]++;
+        for(auto it : matches){
+            mp[it[1]]++;
         }
 
-        vector<int> olost;
-        for (auto it : mp) {
-            if (it.second == 1) {
+        for(auto it : mp){
+            if(it.second == 1){
                 olost.push_back(it.first);
             }
         }
 
-        set<int> st;  
-        vector<int> nlost;
-        for (int i = 0; i < matches.size(); i++) {
-            int winner = matches[i][0];
-            if (mp.find(winner) == mp.end() && st.find(winner) == st.end()) { 
-                nlost.push_back(winner);
-                st.insert(winner);
+        for(auto it : matches){
+            if(mp.find(it[0]) == mp.end() && st.find(it[0]) == st.end()){
+                nlost.push_back(it[0]);
+                st.insert(it[0]);
             }
         }
 
