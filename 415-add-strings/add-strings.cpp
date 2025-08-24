ba11717d53a532854e6f7c306ab4b646ef17778e
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-08-24 23:55:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (60.26%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,20 +1,26 @@
+// https://chatgpt.com/share/68ab58de-008c-8002-9bf0-dc9ecb4460d1
+
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        string ans = "";
         int i = num1.size() - 1;
         int j = num2.size() - 1;
         int carry = 0;
 
-        while (i >= 0 || j >= 0 || carry) {
+        string a = "";
+
+        while(i >= 0 || j >= 0 || carry){
             int sum = carry;
-            if (i >= 0) sum += num1[i--] - '0';
-            if (j >= 0) sum += num2[j--] - '0';
-            ans.push_back((sum % 10) + '0');
+            if(i >= 0) sum += num1[i--] - '0';
+            if(j >= 0) sum += num2[j--] - '0';
+            a.push_back((sum % 10) + '0');
             carry = sum / 10;
         }
 
-        reverse(ans.begin(), ans.end());
-        return ans;
+        reverse(a.begin(), a.end());
+        // a += ans;
+        return a;
     }
 };
+
+
