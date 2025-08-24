5e707db6a12405f5bb30cca3fc364a93b2abdbe8
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-08-25 00:28:23 +0530

    Time: 0 ms (100.00%) | Memory: 76.4 MB (99.12%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,43 +1,38 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* t = dummy;
-        ListNode* temp = dummy;
+        ListNode* tt = l2;  
+        ListNode* temp = l2;
+        ListNode* prev = NULL;  
         int carry = 0;
-        int sum = 0;
 
-        while(l1 || l2){
-            sum = carry;
-            if(l1){
-                sum += l1 -> val;
-                l1 = l1 -> next;
+        while (l1 || l2) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
             }
-            if(l2){
-                sum += l2 -> val;
-                l2 = l2 -> next;
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
             }
 
-            ListNode* nn = new ListNode(sum % 10);
-            temp -> next = nn;
-            temp = temp -> next;
-            carry = sum / 10;
+            if (temp) {
+                temp->val = sum % 10;
+                carry = sum / 10;
+                prev = temp;
+                temp = temp->next;
+            } else {
+                prev->next = new ListNode(sum % 10);
+                prev = prev->next;
+                carry = sum / 10;
+            }
         }
-        if(carry){
-            ListNode* nn = new ListNode(carry);
-            temp -> next = nn;
-            temp = temp -> next;
+
+        if (carry) {
+            prev->next = new ListNode(carry);
         }
-        return t -> next;
+
+        return tt;
     }
 };
